229e9ee076b45da88272ddbc267bfcf2b0d0ba94
Pavel Murygin <pavel.murygin@gmail.com>
2020-12-24 00:16:19 +0300

    Initial commit (barebones react app)

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    <ul>
+                        <li>Create a component</li>
+                        <li>Write some code</li>
+                        <li>Enjoy!</li>
+                    </ul>
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
