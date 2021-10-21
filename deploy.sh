329476b5a98c5098e4dac6ad476e4c14bf24098b
Pavel Murygin <Pavel.Murygin@gmail.com>
2021-10-21 19:07:22 +0300

    use 5th krasnodar node instead of 1st

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-fldist deploy_app --env krasnodar --node-id 12D3KooWSD5PToNiLQwKDXsu8JSysCwUt8BVUJEqCHcDe7P5h45e -i app/app.config.json -o client/src/app.json
+fldist deploy_app --env krasnodar --node-id 12D3KooWFEwNWcHqi9rtsmDhsYcDbRUCDXH84RC4FW6UfsFWaoHi -i app/app.config.json -o client/src/app.json
